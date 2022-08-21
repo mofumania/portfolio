4cd00b3cc5921633cadb93cbc842b6cebad70729
mofumania <108973208+mofumania@users.noreply.github.com>
2022-08-21 17:28:27 +0900

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -1,8 +1,6 @@
 $(".fadeInUpTrigger").on("inview", function (event, isInView) {
   if (isInView) {
     //表示領域に入った時
-    $("#skill .line-anker").addClass(
-      "animate__animated animate__fadeInUp line"
-    ); //クラス名が付与
+    $("#skill .line-anker").addClass("line"); //クラス名が付与
   }
 });
